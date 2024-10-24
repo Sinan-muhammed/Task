b7667c1d6ac49c9619f4cdf0a67a00c43e4aa8b3
Sinan-muhammed <sinasinan418@gmail.com>
2024-10-24 11:42:43 +0530

    top component is compleated

--- src/Top.jsx
+++ src/Top.jsx
@@ -6,33 +6,33 @@ import Features from './cards/Features'
 
 const Top = () => {
   return (
-    <div className='flex items-center justify-center pb-[260px]'>
-      <div className='h-full  w-[50%] flex p-[100px] bg-slate-400 gap-[40px] flex-col'>
-        <div className='pt-[200px]'>
+    <div className='md:flex items-center justify-center md:pb-[260px]'>
+      <div className='h-full  md:w-[50%] flex md:p-[100px] p-[20px] gap-[12px] md:gap-[40px] flex-col'>
+        <div className='md:pt-[200px] pt-[100px] '>
            <div className=' flex gap-3'>
-            <h1 className='text-[40px] font-bold font-poppins'>We make it the way </h1>
-            <img src={Arrow} alt="" />
+            <h1 className='md:text-[40px] text-[25px] font-bold font-poppins'>We make it the way </h1>
+            <img src={Arrow} alt="" className='md:flex hidden' />
            </div>
-           <div className='flex gap-[5px] font-poppins font-bold text-[40px]'>
+           <div className='flex gap-[5px] font-poppins font-bold text-[25px] md:text-[40px]'>
             <h1>your</h1>
             <div className='flex flex-col  '>     
             <h1 className='text-[#B42AB5]'>child</h1>
-            <img src={Horizontal} alt="" />
+            <img src={Horizontal} alt="" className='hidden md:flex'/>
             </div>
             <h1>loves it</h1>
            </div>
         </div>
-        <div className='font-poppins text-[15px] text-[#616161]'>  
-            <h1>Amet minim mollit non deserunt ullamco est sit aliqua dolor do <br /> amet sint. Velit officia consequat duis </h1> 
+        <div className='font-poppins text-[11px] md:text-[15px] text-wrap  text-[#616161]'>  
+            <h1>Amet minim mollit non deserunt ullamco est sit aliqua dolor do <br className='md:flex hidden' /> amet sint. Velit officia consequat duis </h1> 
         </div>
         <div className='flex items-center gap-[20px]'>
-           <div className='h-[56px] w-[56px] bg-[#B42AB5] rounded-full flex items-center justify-center'>
+           <button className='md:h-[56px] md:w-[56px] h-[50px] w-[50px] bg-[#B42AB5] rounded-full flex items-center justify-center'>
             <img src={Play} alt="" />
-           </div>
+           </button>
            <h1 className='text-[#B42AB5] font-poppins font-semibold text-[14px] '>Discover now</h1>
         </div>
       </div>
-      <div className='h-full w-[50%] '>
+      <div className='h-full md:w-[50%] md:p-[90px] md:pt-[190px] pt-[58px]'>
          <Features/>
       </div>
     </div>
--- src/cards/Features.jsx
+++ src/cards/Features.jsx
@@ -1,15 +1,58 @@
 import React from 'react'
 import Yellow from '../../public/yellow.png'
-
+import Guidance from '../../public/Guidance.png'
+import Vertical from '../../public/vertical.png'
+import Experience from  '../../public/Experience.png'
+import Decision from '../../public/Decision.png'
+import Pink from '../../public/pink.png'
 const Features = () => {
   return (
-    <div className='bg-slate-500'>
-        <div>
+    <div className='flex flex-col md:flex-row sm:  md:h-[670px] gap-[6px] md:gap-[20px]'>
+        <div className=' h-full md:w-[40%]  md:flex md:flex-col md:gap-[17px]  items-center md:pl-[39px] justify-center'>
+            <div className='md:flex hidden justify-end  md:w-full '>
             <img src={Yellow} alt="" />
-            <div></div>
-
+            </div>
+            <div className='w-full'>
+            <div className='md:h-[238px] md:w-[218px]  bg-[#344AC1] p-[23px] flex flex-col gap-[22px]'>
+                <div>
+                <img src={Guidance} />
+                </div>
+                <div className='flex flex-col gap-[10px]'>
+                   <h1 className='font-poppins text-white text-[13px] font-semibold'>Guidance</h1>
+                   <h2 className='font-poppins text-white text-[13px] font-light'>Amet minim mollit non <br className='hidden md:flex'/> deserunt ullamco est sit <br className='hidden md:flex'/> aliqua dolor do amet sint. <br className='hidden md:flex'/> Velit officia consequat <br className='hidden md:flex'/> duis enim velit mollit. </h2>
+                </div>
+            </div>           
+               <img src={Vertical} alt="" className='pl-[43%] hidden md:flex'/>
+            </div>
+        </div> 
+        <div className=' md:w-[60%]  md:pr-[90px] flex flex-col md:gap-0 gap-[6px]'>
+               <div className='h-[50%] w-full '>
+                <div className='bg-[#388A7C] p-[30px] flex flex-col gap-[22px]'>
+                <div>
+                 <img src={Experience} alt="" />
+                </div>
+                <div className='flex flex-col gap-[10px]'>
+                    <h1 className='font-poppins text-white text-[13px] font-semibold'>Exploring skills</h1>
+                    <h2 className='font-poppins text-white text-[13px] font-light'>Amet minim mollit non deserunt <br className='hidden md:flex'/> ullamco est sit aliqua dolor do <br className='hidden md:flex'/> amet sint. Velit officia consequat <br className='hidden md:flex'/> duis enim velit mollit. <br className='hidden md:flex'/> 
+Amet minim mollit non deserunt <br className='hidden md:flex'/> ullamco est sit aliqua dolor do <br className='hidden md:flex'/> amet sint. Velit officia consequat <br className='hidden md:flex'/> duis enim velit mollit. </h2>
+                </div>
+                </div>
+               </div>
+               <div className='h-[50%] w-full flex gap-[10%]'>
+                 <div className='md:h-[80%] h-full w-full md:w-[80%] bg-[#151517] p-[26px] flex flex-col gap-[22px]'>
+                    <div>
+                        <img src={Decision} alt="" />
+                    </div>
+                    <div className='flex flex-col gap-[10px]'>
+                        <h1 className='font-poppins text-white text-[13px] font-semibold'>Decision making</h1>
+                        <h2 className='font-poppins text-white text-[13px] font-light'>Amet minim mollit non <br className='hidden md:flex'/> deserunt ullamco est sit <br className='hidden md:flex'/> aliqua dolor do amet sint. <br className='hidden md:flex'/> Velit officia consequat <br className='hidden md:flex'/> duis enim velit mollit. </h2>
+                    </div>
+                 </div>
+                 <div className='md:flex md:flex-col hidden'>
+                    <img src={Pink} alt="" />
+                 </div>
+               </div>
         </div>
-        <div></div>
     </div>
   )
 }
